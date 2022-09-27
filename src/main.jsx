6c0dcc095139947a857b83d6326e3b6d9f811092
Billy Arredondo <billy.arredondo@pucp.pe>
2022-09-26 20:55:11 -0500

    adding BrowserRouter to main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import "./styles.css";
 
+import { BrowserRouter } from "react-router-dom";
 import HeroesApp from "./HeroesApp";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HeroesApp />
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
   </React.StrictMode>
 )
