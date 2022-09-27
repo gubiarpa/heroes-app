2beb7ce33ab65491d20eb50870c7a6b290bdba47
Billy Arredondo <billy.arredondo@pucp.pe>
2022-09-26 22:19:13 -0500

    working AppRouter with Navbar
    working with barrel files

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import { DcPage } from "../heroes/pages/DcPage";
 import { LoginPage } from "../auth/pages/LoginPage";
 import { MarvelPage } from "../heroes/pages/MarvelPage";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
     return (
         <>
+            <Navbar />
             <Routes>
                 <Route path="marvel" element={<MarvelPage />} />
                 <Route path="dc" element={<DcPage />} />
